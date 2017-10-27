5499f58a5f69e6a56d8e8e74eca0e5c768933733
Natalia Savchak <nsavchak@softjourn.com>
2017-10-27 12:26:05 +0300

    [Studio] Delete screen "Login"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -3,13 +3,11 @@ import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu.component';
 import { Www_PhonePortrait } from '../pages/Www/PhonePortrait/Www';
 import { Eee_PhonePortrait } from '../pages/Eee/PhonePortrait/Eee';
-import { Login_PhonePortrait } from '../pages/Login/PhonePortrait/Login';
 export class Screens {
   static declarations: Page[] = [
     MenuComponent,
     Www_PhonePortrait,
-    Eee_PhonePortrait,
-    Login_PhonePortrait
+    Eee_PhonePortrait
   ];
   static mapping = {
     'Www': {
@@ -17,9 +15,6 @@ export class Screens {
     },
     'Eee': {
       PhonePortrait: Eee_PhonePortrait
-    },
-    'Login': {
-      PhonePortrait: Login_PhonePortrait
     }
   }
 }
--- client/src/mocks/index.ts
+++ client/src/mocks/index.ts
@@ -1,3 +1,2 @@
 export * from './Eee/index';
-export * from './Login/index';
 export * from './Www/index';
